f9dabfa09dd2202abc006c269fbfb76b612c979a
DiUrban <nassr_emrani@icloud.com>
2022-09-29 18:48:15 +0100

    Passing test

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
+
 marked.setOptions({
     breaks:true,
 })
 const renderer= new marked.Renderer();
+const initialState=
+`# h1
+
+## h2
+
+[link](google.com)
+
+\`<div></div>\`
+
+\`\`\`
+    let x=1
+\`\`\`
+
+- item1
+
+>Block Quote
+
+![React](https://idsb.tmgrup.com.tr/ly/uploads/images/2021/06/09/120588.jpg)
+
+**Bold**
+
+`
 function App(){
-    const [text,setText]=React.useState('');
+    const [text,setText]=React.useState(initialState);
     return(
             <div className='text-center px-4'>
             <h1 className='p-4'>My Markdown previewer</h1>
             <textarea
             name='text'
-            id='text'
+            id="editor"
             value={text}
             onChange={(e)=>setText(e.target.value)}
             className='textarea'
-            />
-            <h3 className='mt-3'>Output</h3>
+            rows='10'
+            style={{resize:'none'}}
+            ></textarea>
+            <h3 className='mt-3 mb-3'>Output</h3>
             <Preview markdown={text}/>
             </div>
     )
