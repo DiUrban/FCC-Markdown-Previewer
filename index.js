49e6c3eb1518a51e1b4c4b22cc87da7592f7b0b1
DiUrban <nassr_emrani@icloud.com>
2022-09-29 18:21:49 +0100

    fixing wrong marked.js cdn

--- index.js
+++ index.js
@@ -1,40 +1,34 @@
 marked.setOptions({
-    breaks:true
+    breaks:true,
 })
 const renderer= new marked.Renderer();
 function App(){
     const [text,setText]=React.useState('');
     return(
-        <div className='text-center px-4'>
+            <div className='text-center px-4'>
             <h1 className='p-4'>My Markdown previewer</h1>
             <textarea
             name='text'
             id='text'
-            rows='20'
             value={text}
             onChange={(e)=>setText(e.target.value)}
+            className='textarea'
             />
             <h3 className='mt-3'>Output</h3>
             <Preview markdown={text}/>
-        </div>
+            </div>
     )
 }
 
-function Preview(markdown){
+function Preview({markdown}){
     return(
         <div
         dangerouslySetInnerHTML={{
-            __hmtl:marked(markdown,{renderer:renderer})
+            __html:marked(markdown,{renderer:renderer})
         }}
-        id='preview'
-        >
-            
+        id='preview'>  
         </div>
     )
 }
 
-const rootElement=document.getElementById('root');
-const root=ReactDOM.createRoot(rootElement);
-root.render(
-    <App/>
-);
+ReactDOM.render(<App/>,document.getElementById('root'))
